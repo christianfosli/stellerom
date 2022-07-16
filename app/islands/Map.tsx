125a7a586a791fb35d43e4625d7721ece4417006
Christian Fosli <christian.fosli@capgemini.com>
2022-07-16 15:39:49 +0200

    feat: implement go to current location
    * Add feature for navigating to current location.
    * Refactor: Use the official google maps JS API

--- app/islands/Map.tsx
+++ app/islands/Map.tsx
@@ -1,48 +1,86 @@
 /** @jsx h */
-/** @jsxFrag Fragment */
-import { Fragment, h } from "preact";
+import { h } from "preact";
 import { useEffect, useRef, useState } from "preact/hooks";
 import { tw } from "@twind";
-import { Loader } from "https://cdn.pika.dev/google-maps";
+import { Loader } from "@googlemaps/js-api-loader";
 
 interface MapProps {
-  apiKey: string | undefined;
+  apiKey: string;
 }
 
-export default function Map(props: MapProps) {
-  const centerNorway = {
-    lat: 64.68,
-    lng: 9.39,
-  };
+const centerOfNorway = { lat: 64.68, lng: 9.39 };
 
-  const loader = new Loader(
-    props.apiKey,
-    {},
-  );
+export default function MyMap(props: MapProps) {
+  const mapDiv = useRef<HTMLDivElement | null>(null);
 
-  const mapContainer = useRef<HTMLDivElement | null>(null);
-  const [isLoaded, setIsLoaded] = useState(false);
+  // loaded by js-api-loader, I counldn't find out how to get type for typescript
+  // deno-lint-ignore no-explicit-any
+  const [google, setGoogle] = useState<Record<string, any> | null>(null);
+
+  // map type from google maps API. I couldn't figure out how to get type into typescript
+  // deno-lint-ignore no-explicit-any
+  const [map, setMap] = useState<any>(null);
+
+  const [center, setCenter] = useState(centerOfNorway);
+  const [zoom, setZoom] = useState(4);
 
   useEffect(() => {
-    const renderMap = async () => {
-      const google = await loader.load();
-      const map = new google.maps.Map(mapContainer.current, {
-        center: centerNorway,
-        zoom: 4,
+    const load = async () => {
+      const loader = new Loader({
+        apiKey: props.apiKey,
+        version: "weekly",
       });
+
+      const google = await loader.load();
+      console.info("Google maps loaded");
+
+      setGoogle(google);
     };
 
-    renderMap()
-      .then(() => {
-        console.info("Map rendered");
-        setIsLoaded(true);
-      })
-      .catch(console.error);
-  }, []);
+    load().catch(console.error);
+  }, [props.apiKey]);
+
+  useEffect(() => {
+    if (google === null) {
+      return;
+    }
+
+    if (map === null) {
+      setMap(new google.maps.Map(mapDiv.current, { center, zoom }));
+      console.info("Map rendered initially");
+    } else {
+      map.setCenter(center);
+      map.setZoom(zoom);
+      console.info("Map updated");
+    }
+  }, [google, center, zoom]);
+
+  const goToCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setCenter({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+        setZoom(17);
+      },
+      (error) => console.error(error),
+    );
+  };
 
   return (
-    <div ref={mapContainer} class={tw`w-full h-96`}>
-      {isLoaded && <></> || <p>Laster kart...</p>}
+    <div>
+      <div ref={mapDiv} class={tw`w-full h-96`}>
+        {map === null && <p>Laster kart...</p>}
+      </div>
+      <button
+        class={tw
+          `inline-block bg-gray-300 p-2 rounded-md border border-gray-700`}
+        type="button"
+        onClick={goToCurrentLocation}
+      >
+        Naviger til min plassering
+      </button>
     </div>
   );
 }
