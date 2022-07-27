0608be8766dee27f586bd8e99e9dadcc606fa174
Christian Fosli <christian.fosli@capgemini.com>
2022-07-27 20:00:22 +0200

    feat(app): fetch changing rooms from room-api

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,7 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
+import { Handlers, PageProps } from "$fresh/server.ts";
 import Map from "../islands/Map.tsx";
 
 export interface ChangingRoom {
@@ -9,28 +10,32 @@ export interface ChangingRoom {
   location: { lat: number; lng: number };
 }
 
-export default function Home() {
-  const googleMapsApiKey = Deno.env.get("GOOGLE_MAPS_API_KEY") ?? (() => {
-    throw new Error("GOOGLE_MAPS_API_KEY was not set but is required");
-  })();
+const googleMapsApiKey = Deno.env.get("GOOGLE_MAPS_API_KEY") ?? (() => {
+  throw new Error("GOOGLE_MAPS_API_KEY was not set but is required");
+})();
 
-  const mockChangingRooms = [{
-    id: "abc123",
-    name: "Mock stellerom",
-    location: { lat: 60, lng: 10 },
-  }, {
-    id: "abc456",
-    name: "Mock stellerom 2",
-    location: { lat: 65, lng: 11 },
-  }]; // TODO: Fetch from API
+const roomApiUrl = Deno.env.get("ROOM_API_URL") ??
+  "https://room-api-dev.stellerom.no";
 
+export const handler: Handlers<ChangingRoom[]> = {
+  async GET(_, ctx) {
+    const res = await fetch(`${roomApiUrl}/rooms`);
+    if (!res.ok) {
+      console.error(`Non-OK status code from room API: ${res.status}`);
+      return ctx.render([]);
+    }
+    return ctx.render(await res.json());
+  },
+};
+
+export default function Home({ data }: PageProps<ChangingRoom[]>) {
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
       <header>
         <h1 class={tw`text-xl font-light`}>Stellerom.no</h1>
       </header>
       <main>
-        <Map apiKey={googleMapsApiKey} changingRooms={mockChangingRooms} />
+        <Map apiKey={googleMapsApiKey} changingRooms={data} />
       </main>
       <aside class={tw`bg-red-300 my-4 p-2`}>
         <p>
