657b87dbcac2a0f5a5bed2daf740ef0e1c855cf2
Christian Fosli <christian.fosli@capgemini.com>
2024-01-07 22:08:52 +0100

    feat: Add warnings to index.html

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -24,6 +24,31 @@ export default function Home({ data }: PageProps<ChangingRoom[]>) {
   return (
     <div class="p-4 mx-auto max-w-screen-md">
       <Header />
+      <div class="bg-red-900 p-2 text-white">
+        <details>
+          <summary>
+            <h3 class="inline-block text-md font-bold">
+              Driftsmeldinger og greit-å-vite
+            </h3>
+          </summary>
+          <ul class="list-disc p-4">
+            <li class="my-2">
+              Jeg har tenkt å skrive om siden til å hente posisjonsdata om
+              stellerom fra
+              <a href="https://www.openstreetmap.org/">Open Street Map</a>. Det
+              tar en stund siden jeg har prioritert andre ting enn denne
+              nettsiden, men det kan være lurt å legge inn nye og manglende
+              stellerom der 😊.
+            </li>
+            <li class="my-2">
+              Den 1 Februar bytter jeg jobb, og jeg vil i den anledning gjøre
+              litt endringer på Azure infrastuktur som brukes for å hoste
+              backend API'ene for denne nettsiden. Det betyr sannsynligvis ett
+              par dager hvor nettsiden ikke fungerer som forventet.
+            </li>
+          </ul>
+        </details>
+      </div>
       <main>
         <Map apiKey={googleMapsApiKey} changingRooms={data} />
       </main>
