134b08a1e8d0d7858fea37dd030b7f439f4ff567
Christian Fosli <christian.fosli@webstep.no>
2025-04-09 21:41:59 +0200

    Leaflet impl working

--- app/islands/Map.tsx
+++ app/islands/Map.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useRef, useState } from "preact/hooks";
 import { Feature, FeatureCollection } from "geojson";
-import { Layer } from "leaflet";
+import { Layer, LeafletEvent, LeafletEventHandlerFn, Popup } from "leaflet";
 
 interface MapProps {
-  apiKey: string;
   changingRooms: FeatureCollection;
 }
 
@@ -20,11 +19,14 @@ export default function MyMap(props: MapProps) {
   const [L, setL] = useState<any>(null); // work-around for window is not defined during SSR
   // deno-lint-ignore no-explicit-any
   const [map, setMap] = useState<any>(null);
-  // deno-lint-ignore no-explicit-any
+  const [err, setErr] = useState<string>("");
   const [addingChangingRoom, setAddingChangingRoom] = useState<
-    // deno-lint-ignore no-explicit-any
-    { active: boolean; listener: any }
-  >({ active: false, listener: null });
+    {
+      active: boolean;
+      listener: LeafletEventHandlerFn | null;
+      popup: Popup | null;
+    }
+  >({ active: false, listener: null, popup: null });
 
   const leafletLoaded = !(L === null);
 
@@ -74,218 +76,37 @@ export default function MyMap(props: MapProps) {
         : "<p>Ingen anmeldelser</p>";
       layer.bindPopup(`<h3 class="text-lg">${feature.properties.name}</h3>
         ${ratingsHtml}
-        <a href="/rooms/${feature.id}"><button class="p-3 shadow-md">Åpne rom</button></a>`);
+        <a href="/rooms/${feature.id}">
+        <button class="bg-gray-300 p-2 rounded-md border border-gray-700 font-semibold">
+          Gå til rom
+        </button>
+        </a>`);
     };
 
     L.geoJSON(props.changingRooms, {
       onEachFeature,
     }).addTo(mp);
 
+    mp.on(
+      "locationfound",
+      (e: LeafletEvent) => L.circle(e.latlng, e.accuracy).addTo(mp),
+    );
+
+    mp.on("locationerror", (e: LeafletEvent) => setErr(e.message));
+
     setMap(mp);
   }, [leafletLoaded]);
 
   const showCurrentLocation = () => {
-    alert("Not implemented");
+    map.locate({ setView: true, maxZoom: 18 });
   };
 
   const startAddingChangingRoom = () => {
-    alert("Not implemented");
-  };
-
-  const stopAddingChangingRoom = () => {
-    alert("Not implemented");
-  };
-
-  const infoMsg = map === null
-    ? "Laster kart..."
-    : addingChangingRoom.active
-    ? "Legg til stellerom - plasser rommet på kartet"
-    : "Finn og anmeld stellerom";
-
-  return (
-    <div>
-      <p
-        class={`transition-color ease-in-out duration-200 ${
-          addingChangingRoom.active ? "bg-yellow-200" : "bg-transparent"
-        }`}
-      >
-        {infoMsg}
-      </p>
-      <div ref={mapDiv} class="w-full h-96" id="roomsmap">
-      </div>
-      <button
-        class="inline-block bg-gray-300 p-2 rounded-md border border-gray-700"
-        type="button"
-        onClick={showCurrentLocation}
-      >
-        Gå til min plassering
-      </button>
-      {addingChangingRoom.active && (
-            <button
-              class="inline-block bg-yellow-400 p-2 rounded-md border border-gray-700"
-              type="button"
-              onClick={stopAddingChangingRoom}
-            >
-              Avbryt legg til stellerom
-            </button>
-          ) || (
-        <button
-          class="inline-block bg-gray-300 p-2 rounded-md border border-gray-700"
-          type="button"
-          onClick={startAddingChangingRoom}
-        >
-          Legg til nytt stellerom
-        </button>
-      )}
-    </div>
-  );
-}
-
-function _OrigMap(props: MapProps) {
-  const mapDiv = useRef<HTMLDivElement | null>(null);
-
-  // I couldn't figure out how to get objects loaded by Google Maps JavaScript API
-  // into TypeScript types. Therefore some `any`'s below...
-
-  // deno-lint-ignore no-explicit-any
-  const [google, setGoogle] = useState<Record<string, any> | null>(null);
-  // deno-lint-ignore no-explicit-any
-  const [map, setMap] = useState<any>(null);
-  // deno-lint-ignore no-explicit-any
-  const [infoWindow, setInfoWindow] = useState<any>(null);
-  const [addingChangingRoom, setAddingChangingRoom] = useState<
-    // deno-lint-ignore no-explicit-any
-    { active: boolean; listener: any }
-  >({ active: false, listener: null });
-
-  useEffect(() => {
-    // Load google maps JS API
-    const load = async () => {
-      const loader = new Loader({
-        apiKey: props.apiKey,
-        version: "weekly",
-      });
-
-      const google = await loader.load();
-      console.info("Google maps API loaded");
-
-      setGoogle(google);
-    };
-
-    load().catch(console.error);
-  }, [props.apiKey]);
-
-  useEffect(() => {
-    // Initial render of google maps map
-
-    if (google === null) {
-      return;
-    }
-
-    const lastMapPos = localStorage.getItem(localStorageMapPosKey);
-    console.info(`mapPosition loaded from localStorage: ${lastMapPos}`);
-
-    const { center, zoom }: {
-      center: { lat: number; lng: number };
-      zoom: number;
-    } = lastMapPos
-      ? JSON.parse(lastMapPos)
-      : { center: centerOfNorway, zoom: defaultZoom };
-
-    const mp = new google.maps.Map(mapDiv.current, { center, zoom });
-    const iw = new google!.maps.InfoWindow();
-
-    setMap(mp);
-    setInfoWindow(iw);
-
-    const roomMarkers = props.changingRooms.map((r) => {
-      const mark = new google.maps.Marker({
-        position: r.location,
-        // TODO: We can add a custom icon resembling a changing room here
-        map: mp,
-      });
-
-      mark.addListener("click", () => {
-        iw.setContent(
+    const popup = L.popup();
+    const onClick = (e: LeafletEvent) =>
+      popup.setLatLng(e.latlng)
+        .setContent(
           `<div>
-            <h3 class="${tw`text-md font-bold`}">${r.name}</h3>
-            ${
-            r.ratings
-              ? `<ul class="max-w-sm list-none py-2 text-sm">
-                  <li>Tilgjengelighet ${r.ratings.availability}/5</li>
-                  <li>Sikkerhet ${r.ratings.safety}/5</li>
-                  <li>Renslighet ${r.ratings.cleanliness}/5</li>
-                </ul>`
-              : `<p class="text-sm max-w-sm">Ingen anmeldelser enda..</p>`
-          }
-            <a href="/rooms/${r.id}">
-              <button class="bg-gray-300 p-2 rounded-md border border-gray-700">
-                Gå til rom
-              </button>
-            </a>
-          </div>`,
-        );
-        iw.open(mp, mark);
-      });
-
-      return mark;
-    });
-
-    console.info(
-      `Map of ${props.changingRooms.length} rooms rendered`,
-    );
-
-    return () => {
-      roomMarkers.forEach((r) => r.setMap(null));
-      setMap(null);
-    };
-  }, [google, props.changingRooms]);
-
-  useEffect(() => {
-    if (map === null || map === undefined) return;
-
-    const storeMapPos = () => {
-      const { center, zoom } = map;
-      localStorage.setItem(
-        localStorageMapPosKey,
-        JSON.stringify({ center, zoom }),
-      );
-    };
-
-    const timerId = setInterval(storeMapPos, 2000);
-    return () => clearInterval(timerId);
-  }, [map]);
-
-  const showCurrentLocation = () => {
-    navigator.geolocation.getCurrentPosition(
-      (navPosition) => {
-        const position = {
-          lat: navPosition.coords.latitude,
-          lng: navPosition.coords.longitude,
-        };
-        console.info(
-          `Got current location ${JSON.stringify(position)} from browser`,
-        );
-
-        infoWindow.setPosition(position);
-        infoWindow.setContent(
-          `<h3 class="text-md font-bold">Du er her</h3>`,
-        );
-        map.setCenter(position);
-        map.setZoom(17);
-        infoWindow.open(map);
-      },
-      (error) => console.error(error),
-    );
-  };
-
-  const startAddingChangingRoom = () => {
-    // deno-lint-ignore no-explicit-any
-    const listener = map.addListener("click", (evt: any) => {
-      const lat = evt.latLng.lat();
-      const lng = evt.latLng.lng();
-      infoWindow.setContent(
-        `<div>
            <h3 class="text-md font-bold">Nytt stellerom</h3>
            <p>
              Klikk på &quot;Fortsett&quot; om du er fornøyd med plasseringen.
@@ -294,29 +115,26 @@ function _OrigMap(props: MapProps) {
              Appen henter nå også data om stellerom fra <a href="https://www.openstreetmap.org/">OpenStreetMap</a>.
              Vurder å legge inn stellerom der først, med &quot;changing_table=yes&quot; så vil den dukke opp her automatisk ila ett døgn.
            </p>
-            <a href="/new-room?lat=${lat}&lng=${lng}">
-           <button class="bg-gray-300 p-2 rounded-md border border-gray-700">
-          Fortsett
+            <a href="/new-room?lat=${e.latlng.lat}&lng=${e.latlng.lng}">
+           <button class="bg-gray-300 p-2 rounded-md border border-gray-700 font-semibold">
+            Fortsett
           </button>
           </a>
          </div>`,
-      );
-      infoWindow.setPosition({ lat, lng });
-      infoWindow.open(map);
-    });
-    setAddingChangingRoom({ active: true, listener });
+        ).openOn(map);
+    map.on("click", onClick);
+    setAddingChangingRoom({ active: true, listener: onClick, popup: popup });
     console.info("Listening for clicks for adding changing room");
   };
 
   const stopAddingChangingRoom = () => {
-    google!.maps.event.removeListener(addingChangingRoom.listener);
-    console.info("Removed click listener for adding changing rooms");
-
-    if (infoWindow.open) {
-      infoWindow.close();
+    map.off("click", addingChangingRoom.listener);
+    if (addingChangingRoom.popup.isOpen()) {
+      addingChangingRoom.popup.close();
     }
 
-    setAddingChangingRoom({ active: false, listener: null });
+    setAddingChangingRoom({ active: false, listener: null, popup: null });
+    console.info("Removed click listener for adding changing rooms");
   };
 
   const infoMsg = map === null
@@ -334,7 +152,14 @@ function _OrigMap(props: MapProps) {
       >
         {infoMsg}
       </p>
-      <div ref={mapDiv} class="w-full h-96">
+      <p
+        class={`transition-color ease-in-out duration-200 ${
+          err ? "bg-red-400" : "bg-transparent"
+        }`}
+      >
+        {err}
+      </p>
+      <div ref={mapDiv} class="w-full h-96" id="roomsmap">
       </div>
       <button
         class="inline-block bg-gray-300 p-2 rounded-md border border-gray-700"
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,10 +3,6 @@ import Map from "../islands/Map.tsx";
 import Header from "../utils/Header.tsx";
 import { FeatureCollection } from "geojson";
 
-const googleMapsApiKey = Deno.env.get("GOOGLE_MAPS_API_KEY") ?? (() => {
-  throw new Error("GOOGLE_MAPS_API_KEY was not set but is required");
-})();
-
 const roomApiUrl = Deno.env.get("ROOM_API_URL") ??
   "https://room-api-dev.stellerom.no";
 
@@ -26,7 +22,7 @@ export default function Home({ data }: PageProps<FeatureCollection[]>) {
     <div class="p-4 mx-auto max-w-screen-md">
       <Header />
       <main>
-        <Map apiKey={googleMapsApiKey} changingRooms={data} />
+        <Map changingRooms={data} />
       </main>
       <a class="text-blue-700" href="/about">Mer info</a>.
     </div>
