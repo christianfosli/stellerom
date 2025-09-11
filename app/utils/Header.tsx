6078a31d5600eb749fa86db4c23739d59ab7cd85
Christian Fosli <christian.fosli@webstep.no>
2025-09-11 22:03:31 +0200

    feat: Add stellerom logo and update favicon

--- app/utils/Header.tsx
+++ app/utils/Header.tsx
@@ -7,17 +7,27 @@ export default function Header({ isSignedIn, userName }: HeaderProps) {
   return (
     <header class="flex justify-between items-center">
       <a href="/">
-        <h1 class="text-xl font-light">Stellerom.no</h1>
+        <img src="/logo.png" style="max-height: 5rem;" alt="Stellerom logo" />
       </a>
       {isSignedIn && (
             <span>
               <a href="/profile">👤{userName}</a>{" "}
-              <a class="p-2 border rounded" href="/auth/signout">Logg ut</a>
+              <a
+                class="p-2 inline-block whitespace-nowrap border rounded"
+                href="/auth/signout"
+              >
+                Logg ut
+              </a>
             </span>
           ) || (
         <span>
           🫥Ikke innlogget{" "}
-          <a class="p-2 border rounded" href="/auth/signin">Logg inn</a>
+          <a
+            class="p-2 inline-block whitespace-nowrap border rounded"
+            href="/auth/signin"
+          >
+            Logg inn
+          </a>
         </span>
       )}
     </header>
